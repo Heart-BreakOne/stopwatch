dbacfae278f9e046c340d72388d344e9d80a9ef5
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-01-15 18:59:49 -0300

    Cleaning working tree

--- stopwatch/Controller/ViewController.swift
+++ stopwatch/Controller/ViewController.swift
@@ -23,13 +23,18 @@ class ViewController: NSViewController, NSMenuDelegate {
     var transparencyCheck = true
     var isOnTop = true
     
-    
     var time: Timer?
     var startTime: Date?
-    var isTimerRunning: Bool = false
-    var isTimerPaused: Bool = false
-    var elapsedTime: TimeInterval = 0
-
+    var isTimerRunning = false
+    var isTimerPaused  = false
+    var elapsedTime: TimeInterval = 0.0
+    var pausedTime: TimeInterval = 0.0
+    var isStopWatch = false
+    var endTime: Date?
+    var totalElapsedTime: TimeInterval = 0
+    var remainingSeconds: TimeInterval = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -133,18 +138,26 @@ class ViewController: NSViewController, NSMenuDelegate {
         
         //TODO: If time is running, pause it
         if (isTimerRunning) {
-            isTimerPaused = isTimerPaused ? true : false
-            pauseTimer(isTimerPaused: isTimerPaused)
+            pauseTimer()
             return
         }
         //If time is not running and it's zeroed, start stopwatch
         else if !isTimerRunning && (time == "" || time == "00:00:00.000" || time == "00:00:00" || time == "00:00") {
+            isTimerRunning = true
+            isTimerPaused = false
+            isStopWatch = true
+            pausedTime = 0.0
+            totalElapsedTime = 0.0
             runTimer(-1)
             return
         }
         //TODO: Start timer
-        else {
-            
+        else if (!isTimerRunning){
+            isTimerPaused = false
+            isTimerRunning = true
+            isStopWatch = false
+            pausedTime = 0.0
+            totalElapsedTime = 0.0
             let splitTime = time.split(separator: ":")
             var hrs = 0
             var mins = 0
@@ -206,16 +219,42 @@ class ViewController: NSViewController, NSMenuDelegate {
         }
     }
     
-    func pauseTimer(isTimerPaused: Bool) {
-        print(isTimerPaused)
+    func pauseTimer() {
+        print(self.isTimerPaused)
         if isTimerPaused {
-            //TODO: UNPAUSE TIME
-            
-            time = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            if (isStopWatch) {
+                print("unpausing")
+                let tString = timeTextField.stringValue
+                let components = tString.components(separatedBy: ":")
+                    
+                if components.count == 3 {
+                        if let hours = Int(components[0]), let minutes = Int(components[1]) {
+                            let secondsAndMillis = components[2].components(separatedBy: ".")
+                            
+                            if secondsAndMillis.count == 2,
+                               let seconds = Int(secondsAndMillis[0]),
+                               let milliseconds = Int(secondsAndMillis[1]) {
+                                // Calculate the elapsed time in seconds
+                                let elapsedSeconds = TimeInterval(hours * 3600 + minutes * 60 + seconds) + TimeInterval(milliseconds) / 1000.0
+                                
+                                // Set the new start time based on the elapsed time
+                                startTime = Date().addingTimeInterval(-elapsedSeconds)
+                            }
+                        }
+                    }
+                time?.invalidate()
+                time = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateStopwatch), userInfo: nil, repeats: true)
+            }
+            else {
+                //TODO: UNPAUSE TIMER CLOCK
+                print("unpausing")
+                time = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(restartTimer), userInfo: nil, repeats: true)
+            }
             self.isTimerPaused = false
         }
         else {
             //TODO: PAUSE TIME
+            print("pausing")
             time?.invalidate()
             self.isTimerPaused = true
         }
@@ -227,34 +266,100 @@ class ViewController: NSViewController, NSMenuDelegate {
         //Run stopwatch
         if (timer == -1) {
             startTime = Date()
-            
             isTimerRunning = true
             self.isTimerPaused = false
             time?.invalidate()
-            time = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            time = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateStopwatch), userInfo: nil, repeats: true)
             
-        } 
+        }
         //Run timer
         else {
-            
+            endTime = Date().addingTimeInterval(TimeInterval(timer) / 1000.0)
+            time = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         }
     }
     
     @objc func updateTimer() {
-        
-        // Get the current time
+        //print("timer")
+            guard let endTime = endTime else { return }
+            
+            let currentDate = Date()
+            if currentDate >= endTime {
+                isTimerPaused = false
+                isTimerRunning = false
+                timeTextField.stringValue = "00:00:00.000"
+                time?.invalidate()
+                return
+            }
+            
+            let timeDifference = endTime.timeIntervalSince(currentDate)
+            
+            let hours = Int(timeDifference) / 3600
+            let minutes = (Int(timeDifference) % 3600) / 60
+            let seconds = Int(timeDifference) % 60
+            let milliseconds = Int((timeDifference * 1000).truncatingRemainder(dividingBy: 1000))
+            
+        timeTextField.stringValue = String(format: "%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+        }
+
+    
+    @objc func updateStopwatch() {
+        guard let startTime = startTime else {
+            return
+        }
+        //print("stopwatch")
         let currentTime = Date()
+        // Calculate the elapsed time including the time it was paused
+        var elapsedTime = currentTime.timeIntervalSince(startTime)
+        elapsedTime = max(0, elapsedTime)
         
-        // Calculate the elapsed time in seconds
-        elapsedTime = currentTime.timeIntervalSince(self.startTime!)
-        
-        // Convert the elapsed time to the desired format HH:MM:ss.SSS
+        // Convert the elapsed time to hours, minutes, seconds, and milliseconds
         let hours = Int(elapsedTime) / 3600
         let minutes = (Int(elapsedTime) % 3600) / 60
         let seconds = (Int(elapsedTime) % 3600) % 60
         let milliseconds = Int(elapsedTime.truncatingRemainder(dividingBy: 1) * 1000)
         
-        // Update the NSTextField with the timer value
+        // Display the elapsed time in the text field
+        timeTextField.stringValue = String(format: "%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    }
+    
+    
+    @objc func restartTimer() {
+
+        // Handle case when the timer is not paused
+        var tString = timeTextField.stringValue
+        let components = tString.components(separatedBy: ":")
+        self.remainingSeconds = TimeInterval()
+        if components.count == 3 {
+                if let hours = Int(components[0]), let minutes = Int(components[1]) {
+                    let secondsAndMillis = components[2].components(separatedBy: ".")
+                    
+                    if secondsAndMillis.count == 2,
+                       let secs = Int(secondsAndMillis[0]),
+                       let millisecs = Int(secondsAndMillis[1]) {
+                        // Calculate the elapsed time in seconds
+                        self.remainingSeconds = TimeInterval(hours * 3600 + minutes * 60 + secs) + TimeInterval(millisecs) / 1000.0
+                    }
+                }
+        }
+        let currentDate = Date()
+        let newEndTime = currentDate + self.remainingSeconds
+        if currentDate >= newEndTime {
+            print("stopping")
+            isTimerPaused = false
+            isTimerRunning = false
+            timeTextField.stringValue = "00:00:00.000"
+            time?.invalidate()
+            remainingSeconds = 0
+            return
+        }
+        
+        let timeDifference = newEndTime.timeIntervalSince(currentDate)
+        let hours = Int(timeDifference) / 3600
+        let minutes = (Int(timeDifference) % 3600) / 60
+        let seconds = Int(timeDifference) % 60
+        let milliseconds = Int((timeDifference * 1000).truncatingRemainder(dividingBy: 1000)) - 10
+        
         timeTextField.stringValue = String(format: "%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
     }
     
@@ -263,6 +368,7 @@ class ViewController: NSViewController, NSMenuDelegate {
               message: Text("Insert a valid time: HH:MM, HH:MM:SS, HH:MM:SS.sss"),
               dismissButton: .default(Text("OK")))
     }
+    
 }
 
 //Reload user variables after settings page is dismissed.
